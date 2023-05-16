fc09e35d5949a64590dd000fe15fa6f4cd4caa0a
p-box <phako.box@gmail.com>
2023-05-16 07:46:00 +0000

    user_spec.rb ユーザー登録テスト追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー登録' do
+    it 'name, email, password, password_confirmationが存在し登録できる' do
+      user = build(:user)
+      expect(user).to be_valid?
+    end
+
 end
