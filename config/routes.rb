ea01f5bf3f7ee64a2f98d14e91ce6b8a3e995eaa
p-box <phako.box@gmail.com>
2023-05-17 04:27:15 +0000

    usersのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show]
+  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
+  
 end
