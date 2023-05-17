c5a94f6dce493bfd8cc58714b065b1485e88caa1
p-box <phako.box@gmail.com>
2023-05-17 04:21:50 +0000

    spec 編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name                  {'test'}
-    email                 {'test@gmail.com'}
-    password              {'123456'}
+    name {'test'}
+    email {'test@gmail.com'}
+    password {'123456'}
     password_confirmation {'123456'}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
+#テストコードを書くルール
+## 目的はあくまでアプリの完成。多すぎる、難しすぎるテスト＝不要
+## 手作業より効率よくテストができる。
+## 手作業では不安がのこるテスト
+
+# modelテスト
+##簡単なバリデーションは基本無視
+
 
 RSpec.describe User, type: :model do
-  describe 'ユーザー登録' do
+  describe 'create' do
     it 'name, email, password, password_confirmationが存在し登録できる' do
       user = build(:user)
       expect(user).to be_valid
