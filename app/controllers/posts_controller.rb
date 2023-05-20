195263639b999bb4e8d754bebec24d2606473074
p-box <phako.box@gmail.com>
2023-05-20 02:28:26 +0000

    成功メッセージ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,20 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!
 
     def new
         @post = Post.new
     end
 
     def create
+        @user = current_user
+        @post = @user.posts.new(post_params)
+        p @post
+        if @post.save
+            flash[:notice] = "投稿に成功しました"
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
     
     def edit 
@@ -17,6 +27,7 @@ class PostsController < ApplicationController
     end
 
     private
-    def post_params
-    end
+        def post_params
+            params.require(:post).permit(:body)
+        end
 end
