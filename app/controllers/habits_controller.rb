4a0d80e53978aa4e0a8a2015eb956a0c1fbd8e32
p-box <phako.box@gmail.com>
2023-05-21 03:44:07 +0000

    habitcontroller action new create

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,5 +1,36 @@
 class HabitsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @habits = Habit.all
+  end
+
+  def new
+    @habit = Habit.new
+  end
+  
+  def create
+    @user = current_user
+    @habit = @user.habits.new(habit_params)
+    if @habit.save
+      flash[:notice] = "作成しました"
+      redirect_to habits_index_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+  
+  def upadate
+  end
+  
+  def destroy
+  end
+
+  private
+  def habit_params
+    params.require(:habit).permit(:name)
   end
 end
