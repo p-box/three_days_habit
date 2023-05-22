1f84b962f33f8303ae56847594527b43a4e37f9d
p-box <phako.box@gmail.com>
2023-05-22 05:34:05 +0000

    record-migrate

--- db/migrate/20230522052944_create_records.rb
+++ db/migrate/20230522052944_create_records.rb
@@ -1,6 +1,9 @@
 class CreateRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
+      t.datetime :start_time
+      t.references :habit, null: false, foreign_key: true
+
 
       t.timestamps
     end
