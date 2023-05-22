0c7be5e790a5a78c0477089c2d4a5392b794b01a
p-box <phako.box@gmail.com>
2023-05-22 01:36:46 +0000

    default-image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_fit: [50, 50,"Center"]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -14,15 +16,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  
   #
   # def scale(width, height)
   #   # do something
@@ -32,6 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumb do
     process resize_to_fit: [50, 50]
   end
+
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
