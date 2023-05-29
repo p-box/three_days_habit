62901732834fa8af7d579b2cd5fa8053f2bb6d56
p-box <phako.box@gmail.com>
2023-05-29 06:44:42 +0000

    itemと補填が混沌としてきた

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,13 +2,15 @@ class RecordsController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    habit = Habit.find(params[:habit_id])
-    current_record = habit.records.new(record_params)
-    current_record.is_it_continuous(habit)
+    @habit = Habit.find(params[:habit_id])
+    current_record = @habit.records.new(record_params)
+    #初めての記録ではない場合で、継続中か判断し処理する
+    current_record.is_it_continuous(@habit) if 0 < @habit.records.count 
     if current_record.save
       redirect_to request.referer
     else
-      render habit_path(habit), status: :unprocessable_entity
+      render habit_path(@habit)
+      # , status: :unprocessable_entity
     end
   end
   private
--- app/models/record.rb
+++ app/models/record.rb
@@ -9,36 +9,36 @@ class Record < ApplicationRecord
     
     
     def is_it_continuous(habit)
-        yesterday = self.start_time.yesterday
-        latest_record = habit.records.find_by(start_time: yesterday)
-        unless latest_record.nil? 
+        two_records = habit.records.last(2)
+        latest = two_records[0]
+        puts latest.start_time
+        puts self.start_time
+        if latest.start_time == self.start_time.yesterday
+            puts "true"
             # 継続日数を更新
-            self.continuation = latest_record.continuation + 1
+            self.continuation = latest.continuation + 1
+        else
+            # 何日休んでいたか求める
+            diff = (self.start_time - latest.start_time ) / 86400
+            puts diff
+            # 日の差分は当日も含まれるのその分引く
+            holiday = diff.to_i - 1
+            puts holiday
+            remaining = habit.item - holiday
+            puts remaining
+            # 休日を所持しているアイテム数で補填できるか判断
+            if 0 <= remaining
+                # アイテムを消費する
+                habit.update(item: remaining)
+                # 休んだ日のレコードを作成する
+                (1..remaining).each do |i|
+                    no_record_day = habit.records.create(
+                        start_time: latest.start_time + i.day,
+                        continuation: latest.continuation + i
+                    )
+                end
+            end
         end
     end
 
-
 end
-# アイテムで補填が聞く場合のメソッド
-            # diff = (latest_record.start_time - current_record.start_time) / 86400
-            # holiday = diff.to_i
-            # item = record.items
-            # if holiday <= my_item
-            # # 休日をアイテムで補填できる場合
-            # # アイテムを消費する処理
-            # item.update(
-
-            # )
-            # # 補填するレコードの作成
-            #     holiday.times do |i|
-            #     compensation = @habit.records.new(
-            #         start_time: latest_record.start_time + i.day
-            #         continuation: latest_record.continuation + i
-            #     )
-            #     conpensation.save
-            #     end
-                #current_record.continuation = latest_record.continuation + holiday
-            # else
-            # # アイテムでも補填ができず継続日数をリセット
-            #     current_record.continuation = 1
-            # end
